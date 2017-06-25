18c5fffdca7e29d258c52bbd089a9aacbc12f6c5
Esref <esref@esrefs-iMac.local>
2017-06-25 10:16:45 -0700

    Init project

---
+++ test/fixtures/preset-options/spec/expected.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var _this = undefined;
+"1".concat(a);
+
+(function () {
+  babelHelpers.newArrowCheck(this, _this);
+}).bind(undefined);
+
+function a() {
+  var _this2 = this;
+
+  (function () {
+    babelHelpers.newArrowCheck(this, _this2);
+  }).bind(this);
+}
